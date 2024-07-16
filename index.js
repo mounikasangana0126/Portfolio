470224815ebaf91511638c68852d96e0594e835f
Mounika Sangana <sssmounika5@gmail.com>
2024-07-16 14:15:10 +0530

    updated

--- index.js
+++ index.js
@@ -42,20 +42,20 @@ navbar.classList.remove('active');
 
 
 // swiper //
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 50,
-    loop: true,
-    grabCursor: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-});
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 1,
+//     spaceBetween: 50,
+//     loop: true,
+//     grabCursor: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+// });
 
 
 // dark light mode //
@@ -100,7 +100,7 @@ function print(phrase) {
         setTimeout(function () {
             print(phrase)
         }, p);
-    }
+    } 
 }
 function clear() {
     
